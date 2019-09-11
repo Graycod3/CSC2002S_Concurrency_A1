8789cd582aa17df75d7441205a224e292e99febc
Blessing Ngorima <ngrble001@myuct.ac.za>
2019-09-12 01:49:39 +0200

    add Vector java file

---
+++ Vector.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Vector {
+   
+   float x;
+   float y;
+   
+   public Vector(){
+      x=0;
+      y=0;
+   }
+   public Vector(float xv, float yv){
+   
+       x = xv;
+       y = yv;
+
+   }
+    
+}
